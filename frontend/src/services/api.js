47f5c5ed006e77574b0a746adc064534378ab56c
Chandrashekhar <chandrashekhar7215@gmail.com>
2026-05-14 12:28:40 +0530

    Fix production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://civic-report-issue-mern.onrender.com/api",
+  baseURL: "https://civic-report-issue-mern.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
