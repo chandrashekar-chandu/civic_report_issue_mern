7c802f13b603d721ebbcf2b1fda78301de41a060
Chandrashekhar <chandrashekhar7215@gmail.com>
2026-08-28 14:36:03 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://civic-report-issue-mern-1.onrender.com/api",
-   baseURL: "http://localhost:5002/api",
+  baseURL: "https://civic-report-issue-mern-1.onrender.com/api",
+  //  baseURL: "http://localhost:5002/api",
   timeout: 30000,
 });
 
